77990a04e1c6d1167746783b4e38dce4ca32b2eb
MiguelGuzmanMena <64236325+MiguelGuzmanMena@users.noreply.github.com>
2020-05-16 22:21:40 -0600

    fitst c

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Parcial.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
